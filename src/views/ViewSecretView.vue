f23a7a70e687c136093b33cbcb2103bf505eada4
MrLee <52550222+macklee6@users.noreply.github.com>
2025-10-06 11:28:41 +0800

    Update ViewSecretView.vue

--- src/views/ViewSecretView.vue
+++ src/views/ViewSecretView.vue
@@ -104,7 +104,7 @@
 import { ref, onMounted, computed, watch, nextTick } from 'vue';
 import { cryptoService } from '../services/cryptoService';
 import { apiService } from '../services/apiService';
-import { markdownService } from '../services/sanitizedMarkdownService';
+import { markdownService } from '../services/markdownService';
 const props = defineProps({
   secretId: { type: String, required: true }
 });
